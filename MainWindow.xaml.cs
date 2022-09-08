a35a4270925942c3d03b66f5a9c30878495654fd
MaksimK30 <makskuzmih@mail.ru>
2022-09-08 20:33:13 +0300

    Работа

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kuzmich
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string CT;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            Head.Background = new SolidColorBrush(Color.FromRgb(255, 204, 153));
+            EnterBtn.Background = new SolidColorBrush(Color.FromRgb(204, 102, 0));
+            GuestBtn.Background = new SolidColorBrush(Color.FromRgb(204, 102, 0));
+            Captcha.CreateCaptcha(EasyCaptcha.Wpf.Captcha.LetterOption.Alphanumeric, 4);
+            CT = Captcha.CaptchaText;
+        }
+
+        bool CheckCap = false;
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var context = new TradeEntities();
+            var User = context.User.Where(u=>u.UserLogin == ULogin.Text && u.UserPassword == UPwd.Password).FirstOrDefault();
+            
+            if (User != null)
+            {
+                if (!CheckCap)
+                {
+                    var W = new ShopWindow(User);
+                    this.Close();
+                    W.Show();
+                }
+                else if (CheckCap && CT == CaptchaAnsw.Text)
+                {
+                    var W = new ShopWindow(User);
+                    this.Close();
+                    W.Show();
+                }
+                else
+                {
+                    Captcha.CreateCaptcha(EasyCaptcha.Wpf.Captcha.LetterOption.Alphanumeric, 4);
+                    CT = Captcha.CaptchaText;
+                    timerfunc();
+                }
+                
+
+            }
+            else
+            {
+                CheckCap = true;
+                Captcha.Visibility = Visibility;
+                CaptchaAnsw.Visibility = Visibility;
+                Error.Text = "Ошибка !";
+                timerfunc();
+
+
+            }
+        }
+
+         async void timerfunc()
+        {
+            window.IsEnabled = false;
+            await Task.Delay(10000);
+            window.IsEnabled = true;
+        }
+
+        private void GuestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            User usr = new User()
+            {
+                UserRole = 1,
+            };
+
+            var W = new ShopWindow(usr);
+            this.Close();
+            W.Show();
+        }
+    }
+}
---
+++ ShopWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kuzmich
+{
+    /// <summary>
+    /// Логика взаимодействия для ShopWindow.xaml
+    /// </summary>
+    public partial class ShopWindow : Window
+    {
+        string RoleName;
+        public ShopWindow(User usr)
+        {
+            InitializeComponent();
+
+            BinBtn.Background = new SolidColorBrush(Color.FromRgb(204, 102, 0));
+            ChangeBtn.Background = new SolidColorBrush(Color.FromRgb(204, 102, 0));
+            ExitBtn.Background = new SolidColorBrush(Color.FromRgb(204, 102, 0));
+            Head.Background = new SolidColorBrush(Color.FromRgb(255, 204, 153));
+
+
+            switch (usr.UserRole)
+            {
+                case 1:
+                    RoleName = RoleName = " (Гость)";
+                    break;
+
+                case 2:
+                    RoleName = " (Клиент)";
+                    break;
+
+                case 3:
+                    RoleName = " (Менеджер)";
+                    ChangeBtn.Visibility = Visibility.Visible;
+                    break;
+
+                case 4:
+                    RoleName = " (Администратор)";
+                    ChangeBtn.Visibility = Visibility.Visible;
+                    break;
+
+
+            }
+            UName.Text = usr.UserName + " " + usr.UserSurname + RoleName;
+        }
+
+    
+
+    private void ExitBtn_Click(object sender, RoutedEventArgs e)
+    {
+            var W = new MainWindow();
+            this.Close();
+            W.Show();
+    }
+
+    private void ChangeBtn_Click(object sender, RoutedEventArgs e)
+    {
+            var W = new ChangeWindow();
+            this.Close();
+            W.Show();
+        }
+
+    private void BinBtn_Click(object sender, RoutedEventArgs e)
+    {
+            var W = new BinWindow();
+            this.Close();
+            W.Show();
+        }
+}
+}
